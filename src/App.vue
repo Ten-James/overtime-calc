cedfa14168306222684c3484484b51dece0dc8e4
ten-james <hexaplay2@gmail.com>
2023-03-20 08:29:48 +0100

    Resetting and Remove negative time

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ const timeToMinutes = (time) => {
   return times[0] * 60 + times[1] * 1;
 }
 
-const minutesToTime = (minutes) => `00${Math.floor(minutes / 60)}`.slice(-2)+ ":" + `00${minutes % 60}`.slice(-2);
+const minutesToTime = (minutes) => `00${Math.floor(minutes / 60)}`.slice(-2) + ":" + `00${minutes % 60}`.slice(-2);
 
 
 
@@ -34,28 +34,42 @@ export default {
     CalculateTimeBefore() {
       if (this.inputTime === 0)
         return;
-      const ariveMinutes = timeToMinutes(this.arriveTime);
+      const arriveMinutes = timeToMinutes(this.arriveTime);
       const inputMinutes = timeToMinutes(this.inputTime);
-      const diff = inputMinutes - ariveMinutes - Math.floor(Math.random()*5);
+      let diff = inputMinutes - arriveMinutes - Math.floor(Math.random() * 5);
+      if (this.remainingTime < diff)
+        diff = parseInt(this.remainingTime);
       this.remainingTime -= diff;
       this.remainingRealTime = minutesToTime(this.remainingTime);
       this.savedTime = diff;
       this.timeToWrite = minutesToTime(inputMinutes - diff);
       this.inputTime = '00:00'
-      
+
     },
     CalculateTimeAfter() {
       if (this.inputTime === 0)
         return;
       const departmentMinutes = timeToMinutes(this.departmentTime);
       const inputMinutes = timeToMinutes(this.inputTime);
-      const diff = departmentMinutes - inputMinutes - Math.floor(Math.random()*10);
+      let diff = departmentMinutes - inputMinutes - Math.floor(Math.random() * 10);
+      if (this.remainingTime < diff)
+        diff = parseInt(this.remainingTime);
       this.remainingTime -= diff;
       this.remainingRealTime = minutesToTime(this.remainingTime);
       this.savedTime = diff;
       this.timeToWrite = minutesToTime(inputMinutes + diff);
       this.inputTime = '00:00'
-      
+
+    },
+    Reset() {
+      this.wasSet = false;
+      this.arriveTime = '06:00';
+      this.departmentTime = '18:00';
+      this.remainingTime = '10:00';
+      this.remainingRealTime = '00:00';
+      this.inputTime = '00:00';
+      this.savedTime = 0;
+      this.timeToWrite = "00:00";
     }
   },
 }
@@ -64,28 +78,36 @@ export default {
 <template>
   <div v-if="wasSet">
     <p>Zbývá nám {{ remainingRealTime }} přesčasu ({{ remainingTime }} minut).</p>
-    <p>Zadejte čas</p>
-    <input type="time" v-model="inputTime">
-    <p>A je to čas?</p>
-    <div>
-      <button @click="CalculateTimeBefore">Příchodu</button>
-      <button @click="CalculateTimeAfter">Odchodu</button>
+    <div v-if="remainingTime !== 0">
+      <p>Zadejte čas</p>
+      <input type="time" v-model="inputTime">
+      <p>A je to čas?</p>
+      <div>
+        <button @click="CalculateTimeBefore">Příchodu</button>
+        <button @click="CalculateTimeAfter">Odchodu</button>
+      </div>
     </div>
-    <div v-if="savedTime !== 0">
+    <div v-else>
+      <button @click="Reset">
+        Začít znova
+      </button>
+    </div>
+      <div v-if="savedTime !== 0">
         <p>Napište si {{ timeToWrite }}.</p>
         <p>Ušetřili jsme tak {{ savedTime }} minut.</p>
-    </div>
+      </div>
   </div>
   <div v-else>
     <fieldset>
       <legend>Časy pro vstup a výstup</legend>
-    <div>
-      <label for="arriveTime">Nejdřívější možný vstup</label>
-      <input type="time" name="arriveTime" id="arriveTime" v-model="arriveTime">
-    </div>
-    <div>
-      <label for="departmentTime">Nejpozdější možný odchod</label>
-      <input type="time" name="departmentTime" id="departmentTime" v-model="departmentTime"></div>
+      <div>
+        <label for="arriveTime">Nejdřívější možný vstup</label>
+        <input type="time" name="arriveTime" id="arriveTime" v-model="arriveTime">
+      </div>
+      <div>
+        <label for="departmentTime">Nejpozdější možný odchod</label>
+        <input type="time" name="departmentTime" id="departmentTime" v-model="departmentTime">
+      </div>
     </fieldset>
     <p>Zadejte kolik máte přesčasu</p>
     <input type="time" v-model="remainingTime">
@@ -93,14 +115,11 @@ export default {
       <button @click="startTimer">Start</button>
     </div>
   </div>
-  <p
-  class="footer"
-  >by Jakub Indrák 2023</p>
+  <p class="footer">by Jakub Indrák 2023</p>
 </template>
 
 <style scoped>
-
-fieldset > * {
+fieldset>* {
   margin: 0.5rem;
 }
 
